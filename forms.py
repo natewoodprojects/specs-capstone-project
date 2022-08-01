50ea23bd409ab6fa03b8aceba71a6442a18bebc1
Nate Wood <99522059+natewoodprojects@users.noreply.github.com>
2022-08-01 09:20:34 -0500

    all html files made

--- forms.py
+++ forms.py
@@ -1,8 +1,29 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, PasswordField, EmailField 
 
-class SubmitObject(FlaskForm):
+class RegisterUser(FlaskForm):
+    username = StringField("Username: ")
+    password = PasswordField("Password: ")
+    confirm_password = PasswordField("Retype Password: ")
+    email = EmailField("Email: ")
+    register = SubmitField("Register User")
+
+class LoginUser(FlaskForm):
+    username = StringField("Username: ")
+    password = PasswordField("Password: ")
+    login = SubmitField("Login")
+
+class CreateItem(FlaskForm):
     name = StringField("Name of Item ")
     cost = StringField("How many $ did it cost? ")
     hours_desired = StringField("How many hours do you want to put into this? ")
     hours_committed = StringField("How many hours have you made?")
+    submit = SubmitField("Create Item")
+
+class EditItem(FlaskForm):
+    name = StringField("Name of Item ")
+    cost = StringField("How many $ did it cost? ")
+    hours_desired = StringField("How many hours do you want to put into this? ")
+    hours_committed = StringField("How many hours have you made?")
+    edits = SubmitField("Confirm Edits")
+    
--- server.py
+++ server.py
@@ -5,18 +5,29 @@ app = Flask(__name__)
 
 app.secret_key = "39p4uhgau-ewvhoruawe4-9gfhap34u9bp-upsdzv923"
 
-@app.route('/')
+@app.route('/', methods=['POST'])
 def index():
     """index Page"""
 
-    return render_template('/index.html')
+    return render_template('/login.html')
 
-@app.route('/homepage')
-def home():
-    form = SubmitObject()
+@app.route('/register', methods=['POST'])
+def home():    
+    return render_template('/register.html')
 
-    
-    return render_template('/homepage.html')
+@app.route('/home', methods=['GET', 'POST'])
+def home():    
+    return render_template('/home.html')
+
+@app.route('/create', methods=['POST'])
+def home():    
+    return render_template('/create.html')
+
+@app.route('/edit', methods=['POST'])
+def home():    
+    return render_template('/edit.html')
+
+
 
 if __name__ == "__main__":
     app.run()
